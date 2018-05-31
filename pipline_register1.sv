3abc1271cf389602da557a8cadbb20361e9acc21
mhazizian <mohammadh.azizian@gmail.com>
2018-05-31 12:10:58 +0430

    Amend PR1

--- pipline_register1.sv
+++ pipline_register1.sv
@@ -1,20 +1,20 @@
 `include "defines.sv"
 
-module PR_1_IF_ID(clk, rst, PC_in, Instruction_in, PR1_PC, PR1_Instruction);
+module PR_1_IF_ID(clk, rst, PC_in, Instruction_in, PR1_PC_plus1, PR1_Instruction);
 	input clk, rst;
 	input [`INSTRUCTION_LEN - 1:0] Instruction_in;
 	input [`ADDRESS_LEN - 1:0] PC_in;
 
 	output logic [`INSTRUCTION_LEN - 1:0] PR1_Instruction;
-	output logic [`ADDRESS_LEN - 1:0] PR1_PC;
+	output logic [`ADDRESS_LEN - 1:0] PR1_PC_plus1;
 
 	always@(posedge clk, posedge rst) begin
 		if(rst) begin
-			PR1_PC <= 0;
+			PR1_PC_plus1 <= 0;
 			PR1_Instruction <= 0;
 		end
 		else begin
-			PR1_PC <= PC_in;
+			PR1_PC_plus1 <= PC_in;
 			PR1_Instruction <= Instruction_in;
 		end
 	end
