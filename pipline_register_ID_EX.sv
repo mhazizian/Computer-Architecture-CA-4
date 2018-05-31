8f34c348d2cd5144837964633f88f306293d352c
mhazizian <mohammadh.azizian@gmail.com>
2018-05-31 15:05:53 +0430

    Fix bug on bad namingin PR2

--- pipline_register_ID_EX.sv
+++ pipline_register_ID_EX.sv
@@ -1,24 +1,24 @@
 `include "defines.sv"
 
-module PR_2_ID_EX(clk, rsts, PR1_instruction, PR1_RF_out1, PR1_RF_out2, PR1_ALU_ops, PR1_sel_ALU_src_reg2s, 
-	PR1_sel_ALU_src_consts, PR1_sel_PC_src_offsets, PR1_sel_PC_src_consts, PR1_sel_PC_src_plus1s, 
-	PR1_sel_PC_src_stacks, PR1_MEM_writes, PR1_MEM_reads, PR1_sel_RF_write_src_ALUs, PR1_sel_RF_write_src_MEMs,
-	PR1_sel_RF_read_reg2_srcs, PR1_RF_write_ens, PR1_sel_Cin_alus, PR1_push_stacks, PR1_pop_stacks,
+module PR_2_ID_EX(clk, rst, PR1_instruction, PR1_RF_out1, PR1_RF_out2, PR1_ALU_op, PR1_sel_ALU_src_reg2, 
+	PR1_sel_ALU_src_const, PR1_sel_PC_src_offset, PR1_sel_PC_src_const, PR1_sel_PC_src_plus1, 
+	PR1_sel_PC_src_stack, PR1_MEM_write, PR1_MEM_read, PR1_sel_RF_write_src_ALU, PR1_sel_RF_write_src_MEM,
+	PR1_sel_RF_read_reg2_src, PR1_RF_write_en, PR1_sel_Cin_alu, PR1_push_stack, PR1_pop_stack,
 	PR1_sel_ALU_src_shift_count
 
 	// outputs:
 	PR2_instruction, PR2_RF_out1, PR2_RF_out2,
-	PR2_sel_ALU_src_reg2s, PR2_sel_ALU_src_consts, PR2_sel_PC_src_offsets, PR2_sel_PC_src_consts, 
-	PR2_sel_PC_src_plus1s, PR2_sel_PC_src_stacks, PR2_MEM_writes, PR2_MEM_reads, PR2_sel_RF_write_src_ALUs, 
-	PR2_sel_RF_write_src_MEMs, PR2_sel_RF_read_reg2_srcs,  PR2_RF_write_ens, PR2_sel_Cin_alus,
-	PR2_push_stacks, PR2_pop_stacks, PR2_sel_ALU_src_shift_count;
+	PR2_sel_ALU_src_reg2, PR2_sel_ALU_src_const, PR2_sel_PC_src_offset, PR2_sel_PC_src_const, 
+	PR2_sel_PC_src_plus1, PR2_sel_PC_src_stack, PR2_MEM_write, PR2_MEM_read, PR2_sel_RF_write_src_ALU, 
+	PR2_sel_RF_write_src_MEM, PR2_sel_RF_read_reg2_src,  PR2_RF_write_en, PR2_sel_Cin_alu,
+	PR2_push_stack, PR2_pop_stack, PR2_sel_ALU_src_shift_count;
 
 
-	input clk, rst;
-	input PR1_sel_ALU_src_reg2s, PR1_sel_ALU_src_consts, PR1_sel_PC_src_offsets, PR1_sel_PC_src_consts, 
-		PR1_sel_PC_src_plus1s, PR1_sel_PC_src_stacks, PR1_MEM_writes, PR1_MEM_reads, PR1_sel_RF_write_src_ALUs, 
-		PR1_sel_RF_write_src_MEMs, PR1_sel_RF_read_reg2_srcs,  PR1_RF_write_ens, PR1_sel_Cin_alus,
-		PR1_push_stacks, PR1_pop_stacks, PR1_sel_ALU_src_shift_count;
+	input cl, rst;
+	input PR1_sel_ALU_src_reg2, PR1_sel_ALU_src_const, PR1_sel_PC_src_offset, PR1_sel_PC_src_const, 
+		PR1_sel_PC_src_plus1, PR1_sel_PC_src_stack, PR1_MEM_write, PR1_MEM_read, PR1_sel_RF_write_src_ALU, 
+		PR1_sel_RF_write_src_MEM, PR1_sel_RF_read_reg2_src,  PR1_RF_write_en, PR1_sel_Cin_alu,
+		PR1_push_stack, PR1_pop_stack, PR1_sel_ALU_src_shift_count;
 
 
 
@@ -27,41 +27,41 @@ module PR_2_ID_EX(clk, rsts, PR1_instruction, PR1_RF_out1, PR1_RF_out2, PR1_ALU_
 
 	output logic [`INSTRUCTION_LEN - 1:0] PR2_instruction;
 	output logic [`WORD_LEN - 1:0] PR2_RF_out1, PR2_RF_out2;
-	output logic PR2_sel_ALU_src_reg2s, PR2_sel_ALU_src_consts, PR2_sel_PC_src_offsets, PR2_sel_PC_src_consts, 
-		PR2_sel_PC_src_plus1s, PR2_sel_PC_src_stacks, PR2_MEM_writes, PR2_MEM_reads, PR2_sel_RF_write_src_ALUs, 
-		PR2_sel_RF_write_src_MEMs, PR2_sel_RF_read_reg2_srcs,  PR2_RF_write_ens, PR2_sel_Cin_alus,
-		PR2_push_stacks, PR2_pop_stacks, PR2_sel_ALU_src_shift_count;
+	output logic PR2_sel_ALU_src_reg2, PR2_sel_ALU_src_const, PR2_sel_PC_src_offset, PR2_sel_PC_src_const, 
+		PR2_sel_PC_src_plus1, PR2_sel_PC_src_stack, PR2_MEM_write, PR2_MEM_read, PR2_sel_RF_write_src_ALU, 
+		PR2_sel_RF_write_src_MEM, PR2_sel_RF_read_reg2_src,  PR2_RF_write_en, PR2_sel_Cin_alu,
+		PR2_push_stack, PR2_pop_stack, PR2_sel_ALU_src_shift_count;
 
 
-	always@(posedge clk, posedge rst) begin
+	always@(posedge cl, posedge rst) begin
 		if(rst) begin
 			{
-				PR2_instruction, PR2_RF_out1, PR2_RF_out2, PR2_sel_ALU_src_reg2s,
-				PR2_sel_ALU_src_consts, PR2_sel_PC_src_offsets, PR2_sel_PC_src_consts, 
-				PR2_sel_PC_src_plus1s, PR2_sel_PC_src_stacks, PR2_MEM_writes, PR2_MEM_reads, PR2_sel_RF_write_src_ALUs, 
-				PR2_sel_RF_write_src_MEMs, PR2_sel_RF_read_reg2_srcs,  PR2_RF_write_ens, PR2_sel_Cin_alus,
-				PR2_push_stacks, PR2_pop_stacks, PR2_sel_ALU_src_shift_count
+				PR2_instructio, PR2_RF_out, PR2_RF_out, PR2_sel_ALU_src_reg2,
+				PR2_sel_ALU_src_const, PR2_sel_PC_src_offset, PR2_sel_PC_src_const, 
+				PR2_sel_PC_src_plus1, PR2_sel_PC_src_stack, PR2_MEM_write, PR2_MEM_read, PR2_sel_RF_write_src_ALU, 
+				PR2_sel_RF_write_src_MEM, PR2_sel_RF_read_reg2_src,  PR2_RF_write_en, PR2_sel_Cin_alu,
+				PR2_push_stack, PR2_pop_stack, PR2_sel_ALU_src_shift_count
 			} <= 0;
 		end
 		else begin
 			PR2_instruction <= PR1_instruction ;
 			PR2_RF_out1 <= PR1_RF_out1;
 			PR2_RF_out2 <= PR1_RF_out2;
-			PR2_sel_ALU_src_reg2s <= PR1_sel_ALU_src_reg2s;
-			PR2_sel_ALU_src_consts <= PR1_sel_ALU_src_consts;
-			PR2_sel_PC_src_offsets <= PR1_sel_PC_src_offsets;
-			PR2_sel_PC_src_consts <= PR1_sel_PC_src_consts;
-			PR2_sel_PC_src_plus1s <= PR1_sel_PC_src_plus1s;
-			PR2_sel_PC_src_stacks <= PR1_sel_PC_src_stacks;
-			PR2_MEM_writes <= PR1_MEM_writes;
-			PR2_MEM_reads <= PR1_MEM_reads;
-			PR2_sel_RF_write_src_ALUs <= PR1_sel_RF_write_src_ALUs;
-			PR2_sel_RF_write_src_MEMs <= PR1_sel_RF_write_src_MEMs;
-			PR2_sel_RF_read_reg2_srcs <= PR1_sel_RF_read_reg2_srcs;
-			PR2_RF_write_ens <= PR1_RF_write_ens;
-			PR2_sel_Cin_alus <= PR1_sel_Cin_alus;
-			PR2_push_stacks <= PR1_push_stacks;
-			PR2_pop_stacks <= PR1_pop_stacks;
+			PR2_sel_ALU_src_reg2 <= PR1_sel_ALU_src_reg2;
+			PR2_sel_ALU_src_const <= PR1_sel_ALU_src_const;
+			PR2_sel_PC_src_offset <= PR1_sel_PC_src_offset;
+			PR2_sel_PC_src_const <= PR1_sel_PC_src_const;
+			PR2_sel_PC_src_plus1 <= PR1_sel_PC_src_plus1;
+			PR2_sel_PC_src_stack <= PR1_sel_PC_src_stack;
+			PR2_MEM_write <= PR1_MEM_write;
+			PR2_MEM_read <= PR1_MEM_read;
+			PR2_sel_RF_write_src_ALU <= PR1_sel_RF_write_src_ALU;
+			PR2_sel_RF_write_src_MEM <= PR1_sel_RF_write_src_MEM;
+			PR2_sel_RF_read_reg2_src <= PR1_sel_RF_read_reg2_src;
+			PR2_RF_write_en <= PR1_RF_write_en;
+			PR2_sel_Cin_alu <= PR1_sel_Cin_alu;
+			PR2_push_stack <= PR1_push_stack;
+			PR2_pop_stack <= PR1_pop_stack;
 			PR2_sel_ALU_src_shift_count <= PR1_sel_ALU_src_shift_count;
 		end
 	end
