abc6ff4fb3a185d505fa2dcdb09f231cfefc5e70
mhazizian <mohammadh.azizian@gmail.com>
2018-06-06 11:31:22 +0430

    Add forwarding_unit

---
+++ forwarding_unit.sv
@@ -0,0 +1,33 @@
+`include "defines.sv"
+
+module forwarding_unit (
+	PR3_RF_write_en, PR3_MEM_read, PR3_rd,
+	PR4_RF_write_en, PR4_rd,
+	PR2_rs, PR2_rt,
+	forwardA, forwardB
+);
+	input PR3_RF_write_en, PR4_RF_write_en, PR3_MEM_read;
+	input [2:0] PR3_rd, PR4_rd, PR2_rs, PR2_rt;
+
+	output logic [1:0] forwardA, forwardB;
+
+	always@(*) begin
+		forwardA = 0;
+		forwardB = 0;
+
+		if (PR3_RF_write_en & ~PR3_MEM_read) begin
+			if (PR3_rd == PR2_rs)
+				forwardA = 2;
+			if (PR3_rd == PR2_rt)
+				forwardB = 2;
+
+		end else if (PR4_RF_write_en) begin
+			if ((PR4_rd == PR2_rs) & (~PR3_RF_write_en | PR3_rd != PR2_rs))
+				forwardA = 1;
+			if ((PR4_rd == PR2_rt) & (~PR3_RF_write_en | PR3_rd != PR2_rt))
+				forwardB = 1;
+		end
+
+	end
+
+endmodule
